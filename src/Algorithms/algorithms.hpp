bae11ce752960ffb90437a193ac25cfee6f5fdfd
Pascal Menuet <menuet@users.noreply.github.com>
2015-09-03 21:46:21 +0200

    test algorithm "partition"

--- src/Algorithms/algorithms.hpp
+++ src/Algorithms/algorithms.hpp
@@ -30,6 +30,23 @@ namespace algo {
             return next;
         }
 
+        template<class BidirIt, class UnaryPredicate>
+        BidirIt partition(BidirIt first, BidirIt last, UnaryPredicate p)
+        {
+            while (1) {
+                while ((first != last) && p(*first)) {
+                    ++first;
+                }
+                if (first == last--) break;
+                while ((first != last) && !p(*last)) {
+                    --last;
+                }
+                if (first == last) break;
+                std::iter_swap(first++, last);
+            }
+            return first;
+        }
+
     } // namespace cppref
 
     namespace mine {
@@ -51,6 +68,26 @@ namespace algo {
                 return mine::rotate(left, n_first, last);
 
             return mine::rotate(n_first, right, last);
+        }
+
+        template<class FwdIt, class UnaryPredicate>
+        FwdIt partition(FwdIt first, FwdIt last, UnaryPredicate p)
+        {
+            while ((first != last) && p(*first))
+                ++first;
+            if (first == last)
+                return first;
+
+            auto ppoint = first++;
+            while (first != last)
+            {
+                if (p(*first))
+                    std::iter_swap(ppoint++, first++);
+                else
+                    ++first;
+            }
+
+            return ppoint;
         }
 
     } // namespace mine
--- src/Algorithms/algorithms.test.cpp
+++ src/Algorithms/algorithms.test.cpp
@@ -34,19 +34,67 @@ namespace algo {
 
 					THEN("my algorithm produces the same as the reference")
 					{
-						for (size_t pos=0 ; pos<vec.size() ; ++pos)
-						{
-							algo::mine::rotate(begin(vec_mine), begin(vec_mine) + pos, end(vec_mine));
-							algo::cppref::rotate(begin(vec_cppref), begin(vec_cppref) + pos, end(vec_cppref));
-							std::rotate(begin(vec_std), begin(vec_std) + pos, end(vec_std));
-							
-                            REQUIRE(vec_mine == vec_cppref);
-							REQUIRE(vec_mine == vec_std);
-						}
-					}
+                        bool result_cppref = true;
+                        bool result_std = true;
+                        for (size_t pos = 0; pos < vec.size() && result_cppref && result_std; ++pos)
+                        {
+                            auto point_mine = algo::mine::rotate(begin(vec_mine), begin(vec_mine) + pos, end(vec_mine));
+                            auto point_cppref = algo::cppref::rotate(begin(vec_cppref), begin(vec_cppref) + pos, end(vec_cppref));
+                            auto point_std = std::rotate(begin(vec_std), begin(vec_std) + pos, end(vec_std));
+
+                            // TODO: fix returned value
+                            // result_cppref = (point_mine - begin(vec_mine)) == (point_cppref - begin(vec_cppref));
+                            // result_std = (point_mine - begin(vec_mine)) == (point_std - begin(vec_std));
+
+                            result_cppref = result_cppref && vec_mine == vec_cppref;
+                            result_std = result_std && vec_mine == vec_std;
+                        }
+
+                        REQUIRE(result_cppref);
+                        REQUIRE(result_std);
+                    }
 				}
 			}
 		}
+
+        SCENARIO("partition", "[algorithms]")
+        {
+            GIVEN("a vector of random size with random data")
+            {
+                std::uniform_int_distribution<size_t> sizeDistrib(10, 50);
+                std::vector<int> vec(sizeDistrib(gs_mt));
+                std::uniform_int_distribution<int> valDistrib(0, 50);
+                std::generate(begin(vec), end(vec), [&]() { return valDistrib(gs_mt); });
+
+                WHEN("We partition it so that all the even numbers are before the odd numbers")
+                {
+                    auto vec_mine = vec;
+                    auto vec_std = vec;
+                    auto vec_cppref = vec;
+                    auto partitioner = [](auto i) { return i % 2 == 0; };
+
+                    THEN("my algorithm produces the same as the reference (when ignoring non-preserved relative order)")
+                    {
+                        auto point_mine = algo::mine::partition(begin(vec_mine), end(vec_mine), partitioner);
+                        auto point_cppref = algo::cppref::partition(begin(vec_cppref), end(vec_cppref), partitioner);
+                        auto point_std = std::partition(begin(vec_std), end(vec_std), partitioner);
+
+                        REQUIRE((point_mine - begin(vec_mine)) == (point_cppref - begin(vec_cppref)));
+                        REQUIRE((point_mine - begin(vec_mine)) == (point_std - begin(vec_std)));
+
+                        std::sort(begin(vec_mine), point_mine);
+                        std::sort(point_mine, end(vec_mine));
+                        std::sort(begin(vec_cppref), point_cppref);
+                        std::sort(point_cppref, end(vec_cppref));
+                        std::sort(begin(vec_std), point_std);
+                        std::sort(point_std, end(vec_std));
+
+                        REQUIRE(vec_mine == vec_cppref);
+                        REQUIRE(vec_mine == vec_std);
+                    }
+                }
+            }
+        }
 
 	} // namespace ut
 
