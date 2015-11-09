05d036183a34dc3918892eea8d2af4576fc50cc3
Pascal Menuet <menuet@users.noreply.github.com>
2015-11-09 13:32:10 +0100

    Add third-parties boost and mongo

--- src/thirdparties/build.bat
+++ src/thirdparties/build.bat
@@ -1,7 +1,7 @@
 
 set "MSBUILD_PATH=C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
 set "CMAKE_PATH=c:\Program Files (x86)\CMake\bin\cmake.exe"
-set "THIRDPARTIES_DIR=C:\DEV\EXPERIMENTS\THIRDPARTIES"
+set "THIRDPARTIES_DIR=%~dp0..\..\..\THIRDPARTIES"
 set "BUILD_DIR=%THIRDPARTIES_DIR%\build"
 set "SCRIPT_DIR=%~dp0"
 
