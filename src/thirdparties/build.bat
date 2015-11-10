e9803f8b0060923f379adf9e9edcf9d2caab9785
Pascal Menuet <menuet@users.noreply.github.com>
2015-11-10 23:21:21 +0100

    Fix dependencies issues and include directories names

--- src/thirdparties/build.bat
+++ src/thirdparties/build.bat
@@ -1,5 +1,4 @@
 
-set "MSBUILD_PATH=C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
 set "CMAKE_PATH=c:\Program Files (x86)\CMake\bin\cmake.exe"
 set "THIRDPARTIES_DIR=%~dp0..\..\..\THIRDPARTIES"
 set "BUILD_DIR=%THIRDPARTIES_DIR%\build"
@@ -11,8 +10,7 @@ IF NOT EXIST "%BUILD_DIR%" (
 
 cd "%BUILD_DIR%"
 
-"%CMAKE_PATH%" -G "Visual Studio 12 2013" -D "KHEWIL_THIRDPARTIES_DIR=%THIRDPARTIES_DIR%" "%SCRIPT_DIR%" > "%SCRIPT_DIR%\build.logs.txt"
-"%CMAKE_PATH%" --build . --target khewil-thirdparties-build >> "%SCRIPT_DIR%\build.logs.txt"
-"%CMAKE_PATH%" --build . --target khewil-thirdparties-install >> "%SCRIPT_DIR%\build.logs.txt"
+"%CMAKE_PATH%" -G "Visual Studio 12 2013" "%SCRIPT_DIR%" > "%SCRIPT_DIR%\build.logs.txt"
+"%CMAKE_PATH%" --build . --target khewil-thirdparties-build-install >> "%SCRIPT_DIR%\build.logs.txt"
 
 cd "%SCRIPT_DIR%"
