10b1c46ac47d52b369f6ad471ca42b3aaad20a65
Pascal Menuet <pascal.menuet@free.fr>
2017-10-30 20:23:12 +0100

    add mouse for dinner

--- src/snake/snake.cpp
+++ src/snake/snake.cpp
@@ -8,6 +8,16 @@ int main(int argc, char* argv [])
 
     sf::Texture snakeImage;
     snakeImage.loadFromFile(R"(d:\DEV\PERSO\EXPERIMENTS\experiments\src\snake\snake.png)");
+    sf::CircleShape snake(50);
+    snake.setTexture(&snakeImage);
+
+    sf::Texture mouseImage;
+    mouseImage.loadFromFile(R"(d:\DEV\PERSO\EXPERIMENTS\experiments\src\snake\mouse.png)");
+    sf::CircleShape mouse(20);
+    mouse.setTexture(&mouseImage);
+
+    bool isMouseVisible = true;
+
     float x = 600;
     float y = 400;
     for (;;)
@@ -28,15 +38,21 @@ int main(int argc, char* argv [])
                     x = x - 10;
                 else if (event.key.code == sf::Keyboard::Right && x < 1200-100)
                     x = x + 10;
+                if (x <= 100 && x+100 >= 100+40 && y <= 200 && y+100 >= 200+40)
+                {
+                    isMouseVisible = false;
+                }
             }
         }
 
         window.clear(sf::Color{168, 64, 202});
 
-        sf::CircleShape snake(50);
-        snake.setTexture(&snakeImage);
-//        snake.setFillColor(sf::Color(47, 185, 219));
         snake.setPosition(x, y);
+
+        mouse.setPosition(100, 200);
+
+        if (isMouseVisible)
+            window.draw(mouse);
 
         window.draw(snake);
 
