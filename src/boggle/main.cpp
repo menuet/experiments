7b58b300caeee9de08f75c2a4a009aa57aa815a8
Pascal Menuet <pascal.menuet@free.fr>
2018-09-23 16:48:09 +0200

    add mul

--- src/boggle/main.cpp
+++ src/boggle/main.cpp
@@ -1,6 +1,73 @@
 
 #include <random>
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <conio.h>
+
+int random_number(std::default_random_engine& rg, int min = 3, int max = 10)
+{
+    std::uniform_int_distribution<int> uniform_dist(min, max);
+    return uniform_dist(rg);
+}
+
+void show_random_additions(int wait_seconds)
+{
+    std::random_device rd;
+    std::default_random_engine rg(rd());
+
+    for (;;)
+    {
+        const auto left = random_number(rg);
+        const auto right = random_number(rg);
+        std::cout << "Combien font : " << left << " + " << right << " ?\n";
+        //for (int i = 0; i < wait_seconds; ++i)
+        //{
+        //    std::cout << "...\n";
+        //    std::this_thread::sleep_for(std::chrono::seconds(1));
+        //}
+        const auto code = []
+        {
+            const auto first = _getch();
+            if (first != 0)
+                return first;
+            return _getch();
+        }();
+        std::cout << "Reponse : " << left + right << "\n\n";
+//        std::this_thread::sleep_for(std::chrono::seconds(1));
+        if (code == 3)
+            break;
+    }
+}
+
+void show_random_multiplications(int wait_seconds)
+{
+    std::random_device rd;
+    std::default_random_engine rg(rd());
+
+    for (;;)
+    {
+        const auto left = random_number(rg, 0, 5);
+        const auto right = random_number(rg, 0, 5);
+        std::cout << "Combien font : " << left << " x " << right << " ?\n";
+        for (int i = 0; i < wait_seconds; ++i)
+        {
+            std::cout << "...\n";
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        //const auto code = []
+        //{
+        //    const auto first = _getch();
+        //    if (first != 0)
+        //        return first;
+        //    return _getch();
+        //}();
+        std::cout << "Reponse : " << left * right << "\n\n";
+        //        std::this_thread::sleep_for(std::chrono::seconds(1));
+        //if (code == 3)
+        //    break;
+    }
+}
 
 char random_letter(std::default_random_engine& rg)
 {
@@ -8,7 +75,7 @@ char random_letter(std::default_random_engine& rg)
     return uniform_dist(rg);
 }
 
-int main()
+void show_boggle()
 {
     std::random_device rd;
     std::default_random_engine rg(rd());
@@ -22,5 +89,9 @@ int main()
         }
         std::cout << "\n\n";
     }
+}
 
+int main()
+{
+    show_random_multiplications(3);
 }
