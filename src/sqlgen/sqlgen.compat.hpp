2a1012c43da9bc7d8bb60e5ff07bdd926d937ccf
Pascal Menuet <pascal.menuet@free.fr>
2019-08-21 21:03:35 +0200

    try fixing clang on travis/linux too

--- src/sqlgen/sqlgen.compat.hpp
+++ src/sqlgen/sqlgen.compat.hpp
@@ -1,29 +1,27 @@
 
 #pragma once
 
-
 #include <type_traits>
 
-
-#ifndef _MSC_VER
+#if !defined(__cpp_lib_type_trait_variable_templates)
 
 namespace std {
 
-    template< class From, class To >
+    template <class From, class To>
     constexpr bool is_convertible_v = is_convertible<From, To>::value;
 
-    template< class T, class U >
+    template <class T, class U>
     constexpr bool is_same_v = is_same<T, U>::value;
 
-    template< class T, class... Args >
+    template <class T, class... Args>
     constexpr bool is_constructible_v = is_constructible<T, Args...>::value;
 
-    template< class T >
+    template <class T>
     constexpr bool is_arithmetic_v = is_arithmetic<T>::value;
 
-    template< class Base, class Derived >
+    template <class Base, class Derived>
     constexpr bool is_base_of_v = is_base_of<Base, Derived>::value;
 
-}
+} // namespace std
 
 #endif
